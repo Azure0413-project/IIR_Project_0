893e68c3cccd184b48012aed8bd408a7b710eb64
Azure0413-project <azureforweb@gmail.com>
2024-07-31 22:16:05 +0800

    Delete myapp directory

--- myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-from django.contrib.auth.decorators import login_required
-
-urlpatterns = [
-    path('', login_required(views.index_page), name='page'),
-    path('register', views.register, name='register'),
-    path('login', views.login_view, name='login'),
-]
