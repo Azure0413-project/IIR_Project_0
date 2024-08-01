458fed0832884515644c50bdae5d10897c714058
wilsonouo <hand11926@gmail.com>
2024-08-01 10:00:29 +0000

    first

--- project/RAG.py
+++ project/RAG.py
@@ -5,9 +5,6 @@ from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFacePipeline
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
 from langchain.chains import RetrievalQA
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPromptTemplate
 import os
 
 import warnings
@@ -31,13 +28,12 @@ def db_update(db_destination, document_dir, device_choice, chunk_size, chunk_ove
     )
 
     if os.path.exists(db_destination) == True:
-        print(os.path.split(db_destination)[1])
         db = FAISS.load_local(os.path.split(db_destination)[1], embeddings,allow_dangerous_deserialization=True)
     else:
         for i in range(len(document)):
 
             #  Create a loader instance ansd load the PDF in
-            loader = CSVLoader(os.path.join(document_dir,document[i]))
+            loader = CSVLoader(os.path.join(document_dir,document[i]), source_column="Disease")
             data = loader.load()
 
             # It splits text into chunks of 1000 characters each with a 150-character overlap.
@@ -50,7 +46,7 @@ def db_update(db_destination, document_dir, device_choice, chunk_size, chunk_ove
             db = FAISS.from_documents(docs, embeddings)
             
             # save the db
-            db.save_local(os.path.split(db_destination)[1])
+            db.save_local(os.path.split(db_destination)[0])
     
     return db
 
@@ -59,50 +55,36 @@ def RAG(query:str, db_destination=None, document_dir=None,chunk_size=1000, chunk
 
     # get the db
     db = db_update(db_destination=db_destination, document_dir=document_dir, device_choice=device_choice, chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    retriever = db.as_retriever(search_kwargs={"k": 1})
+    retriever = db.as_retriever()
+    print(retriever.get_)
 
+
+    # get the llm
+    # model_name = 'meta-llama/Meta-Llama-3-8B'
     model_name = 'google/flan-t5-base'
-    tokenizer = AutoTokenizer.from_pretrained(model_name, padding=True, truncation=True, max_length=200)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, padding=True, truncation=True, max_length=512)
     text2text = pipeline(
         task = 'text2text-generation',
         model = model_name,
         tokenizer = tokenizer,
-        max_new_tokens = 200,
+        max_new_tokens = 512,
+        device = 'cpu'
     )
 
 
     # Create an instance of the HuggingFacePipeline, which wraps the question-answering pipeline
     # with additional model-specific arguments (temperature and max_length)
-    # llm = HuggingFacePipeline(
-    #     pipeline=text2text,
-    #     model_kwargs={'temperature': 0.7, 'max_new_tokens': 200},
-    # )
-
-
-    # Create a retriever object from the 'db' with a search configuration where it retrieves up to 4 relevant splits/documents.
-    
-
-
-    system_prompt = (
-        "Use the given context to answer the question. "
-        "If you don't know the answer, say you don't know. "
-        "Use three sentence maximum and keep the answer concise. "
-        "Context: {context}"
+    llm = HuggingFacePipeline(
+        pipeline=text2text,
+        model_kwargs={'temperature': 0.7, 'max_new_tokens': 512},
     )
-    prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", system_prompt),
-        ("human", "{input}"),
-    ]
-    )
-    question_answer_chain = create_stuff_documents_chain(text2text, prompt)
-    retrieval_chain = create_retrieval_chain(retriever, question_answer_chain)
-    
-    retrieval_chain.invoke({'input':query})
-
-    # print(docs[0].page_content)
-    print('done')
     
 
+    # It's configured with a language model (llm), a chain type "refine," the retriever we created, and an option to not return source documents.
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="refine", retriever=retriever, return_source_documents=False)
+    a = qa.invoke(query)
 
-RAG('please suggest for me a dish. I want to have chicken, eggs. I am a diabete patient', '/home/project/faiss_index', '/home/project/document')
+    print(a['result'])
+    
+
+RAG('please suggest for me a dish.I am a diabete patient', '/home/project/faiss_index', '/home/project/document')
