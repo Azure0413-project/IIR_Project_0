59623358fd59e41c599941ae501a6a034c5dcb3d
wilsonouo <hand11926@gmail.com>
2024-08-04 10:42:51 +0000

    0804second

--- project/RAG.py
+++ project/RAG.py
@@ -4,7 +4,6 @@ from langchain_community.document_loaders import CSVLoader
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFacePipeline
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
-from langchain.chains import RetrievalQA
 import os
 
 import warnings
@@ -31,22 +30,28 @@ def db_update(db_destination, document_dir, device_choice, chunk_size, chunk_ove
         db = FAISS.load_local(os.path.split(db_destination)[1], embeddings,allow_dangerous_deserialization=True)
     else:
         for i in range(len(document)):
-
-            #  Create a loader instance ansd load the PDF in
-            loader = CSVLoader(os.path.join(document_dir,document[i]), source_column="Disease")
+            
+            # according to different file, use different method
+            if document[i].split()[1] == 'csv':
+                loader = CSVLoader(os.path.join(document_dir,document[i]), source_column="Disease")
+            elif document[i].split()[1] == 'pdf':
+                loader = PyMuPDFLoader(os.path.join(document_dir,document[i]))
+            
+            # load the data
             data = loader.load()
 
             # It splits text into chunks of 1000 characters each with a 150-character overlap.
             text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-
-            # split the text into documents using the text splitter.
             docs = text_splitter.split_documents(data)
 
             # create the db
-            db = FAISS.from_documents(docs, embeddings)
+            if i != 0:
+                db = db.merge_from(FAISS.from_documents(docs, embeddings))
+            else:
+                db = FAISS.from_documents(docs, embeddings)
             
-            # save the db
-            db.save_local(os.path.split(db_destination)[0])
+        # save the db
+        db.save_local(os.path.split(db_destination)[1])
     
     return db
 
@@ -56,35 +61,43 @@ def RAG(query:str, db_destination=None, document_dir=None,chunk_size=1000, chunk
     # get the db
     db = db_update(db_destination=db_destination, document_dir=document_dir, device_choice=device_choice, chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     retriever = db.as_retriever()
-    print(retriever.get_)
 
 
     # get the llm
     # model_name = 'meta-llama/Meta-Llama-3-8B'
     model_name = 'google/flan-t5-base'
-    tokenizer = AutoTokenizer.from_pretrained(model_name, padding=True, truncation=True, max_length=512)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, padding=True, truncation=True, max_length=2000)
     text2text = pipeline(
         task = 'text2text-generation',
         model = model_name,
         tokenizer = tokenizer,
-        max_new_tokens = 512,
+        max_new_tokens = 2000,
         device = 'cpu'
     )
 
-
-    # Create an instance of the HuggingFacePipeline, which wraps the question-answering pipeline
-    # with additional model-specific arguments (temperature and max_length)
-    llm = HuggingFacePipeline(
-        pipeline=text2text,
-        model_kwargs={'temperature': 0.7, 'max_new_tokens': 512},
+    # prompt
+    from langchain.prompts import PromptTemplate
+    location_extractor_prompt = PromptTemplate(
+        input_variables=['dish', 'ingredient'],
+        template=
+        """
+        I recommand {dish}, and the preparing is following:{ingredient}
+        """
     )
+
+    # retrive form the db
+    col = retriever.get_relevant_documents(query)[0].page_content.split('\n')
+    ingredient = col[3].split(':')[1]
+    dish = col[1].split(':')[1]
+
+    # combine with the prompt
+    prompt_text = location_extractor_prompt.format(dish=dish, ingredient=ingredient)
+
+    # put the prompt into llm
+    llmresult = text2text(prompt_text)
+
+    return {'dish': dish, 'response': prompt_text}
+    
     
 
-    # It's configured with a language model (llm), a chain type "refine," the retriever we created, and an option to not return source documents.
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="refine", retriever=retriever, return_source_documents=False)
-    a = qa.invoke(query)
-
-    print(a['result'])
-    
-
-RAG('please suggest for me a dish.I am a diabete patient', '/home/project/faiss_index', '/home/project/document')
+RAG('please suggest for me a dish', '/home/project/faiss_index', '/home/project/document')
