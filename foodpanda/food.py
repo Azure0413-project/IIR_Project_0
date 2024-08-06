5a6bd0f988600883c9b274137bff8b40662f30da
Azure0413-project <azureforweb@gmail.com>
2024-08-06 11:55:43 +0800

    Update food.py

--- foodpanda/food.py
+++ foodpanda/food.py
@@ -10,11 +10,9 @@ import os
 
 # change the url you want to get
 # if catch item occur error, please change the pauseTimem and srollHeight below. roll_web(Line:48). smaller is recommanded. 
-url = "https://www.foodpanda.com.tw/restaurant/a9is/jia-andb-coffee-takeaway-bar-tai-nan-guang-ming-dian"
+url = "https://www.foodpanda.com.tw/restaurant/fkq7/ding-ding-xian-chao-zha-cai-rou-si-mian-xiao-bei-dian"
 # you need to change id every round
-id = 0
-
-
+id = 1012
 
 def roll_web(driver, pauseTime = 1, srollHeight = 300, final_stop = 2):
     SCROLL_PAUSE_TIME = pauseTime
@@ -24,19 +22,15 @@ def roll_web(driver, pauseTime = 1, srollHeight = 300, final_stop = 2):
     now_height = 0
 
     while now_height < last_height:
-        # Scroll down to bottom
         driver.execute_script("window.scrollBy(0, " + str(srollHeight) + ");")
-        # Wait to load page
         time.sleep(SCROLL_PAUSE_TIME)
         now_height += srollHeight
     
     time.sleep(final_stop)
 
-
 def crawler(url, id):
-    # web address (TaiwanGoodInfo)
     
-    # pretent it from the normal user
+    # pretend it is from a normal user
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'}
 
     # get the website
@@ -47,54 +41,47 @@ def crawler(url, id):
     _ = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "lazy-loaded-dish-photo")))
     roll_web(driver, pauseTime = 1, srollHeight = 300, final_stop = 5)
 
-
     # analyze the web
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
     # get the vendor name
     vendor = soup.find("h1", class_="sm:f-title-medium-font-size md:f-title-medium-font-size lg:f-title-medium-font-size xl:f-title-large-font-size f-title-xlarge-font-size sm:fw-title-medium-font-weight md:fw-title-medium-font-weight lg:fw-title-medium-font-weight xl:fw-title-large-font-weight fw-title-xlarge-font-weight sm:lh-title-medium-line-height md:lh-title-medium-line-height lg:lh-title-medium-line-height xl:lh-title-large-line-height lh-title-xlarge-line-height sm:ff-title-medium-font-family md:ff-title-medium-font-family lg:ff-title-medium-font-family xl:ff-title-large-font-family ff-title-xlarge-font-family sm:ffs-title-medium-font-feature-settings md:ffs-title-medium-font-feature-settings lg:ffs-title-medium-font-feature-settings xl:ffs-title-large-font-feature-settings ffs-title-xlarge-font-feature-settings")
-    data_dir = os.path.join(os.getcwd(),"data")
-    
+
     # create the folder
-    vendor_dir = os.path.join(data_dir,vendor.text)
-    os.makedirs(vendor_dir, exist_ok = True)
+    data_dir = os.path.join(os.getcwd(), "data")
+    os.makedirs(data_dir, exist_ok=True)
 
     # write into csv
-    csv_path = os.path.join(vendor_dir, "vendor.csv")
-    with open(csv_path, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-
-        writer.writerow(['id', 'vendor', 'product_name', 'product_price', 'product_img'])
-
+    csv_path = os.path.join(data_dir, "vendor.csv")
+    if not os.path.exists(csv_path):
+        with open(csv_path, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['id', 'vendor', 'product_name', 'product_price', 'product_img'])
 
     # get the product
     product_grid = soup.find_all("li", class_="box-flex product-tile fd-row p-relative pa-st")
 
-
-    for grid in product_grid:
-
-        name = grid.find("span", class_="vertical-align-middle")
-        price = grid.find("p", class_="cl-neutral-primary f-paragraph-medium-font-size fw-paragraph-medium-font-weight lh-paragraph-medium-line-height ff-paragraph-medium-font-family")
-        price = price.text[price.text.find('$')+1:]
-        # print(pic["style"].split("\"")[1])
-        # download the pic
-        pic = grid.find("div", class_="lazy-loaded-dish-photo")
-        pic_url = pic["style"].split("\"")[1]
-        if(pic["style"] != None):
-            pic_path = os.path.join(vendor_dir, str(id) + ".jpeg")
+    try:
+        for grid in product_grid:
+            name = grid.find("span", class_="vertical-align-middle")
+            price = grid.find("p", class_="cl-neutral-primary f-paragraph-medium-font-size fw-paragraph-medium-font-weight lh-paragraph-medium-line-height ff-paragraph-medium-font-family")
+            price = price.text[price.text.find('$')+1:]
+            pic = grid.find("div", class_="lazy-loaded-dish-photo")
+            pic_url = pic["style"].split("\"")[1]
+            
+            # download the pic
+            pic_path = os.path.join(data_dir, str(id) + ".jpeg")
             with open(pic_path, "wb") as f:
                 f.write(requests.get(pic_url).content)
 
+            # write into csv
+            with open(csv_path, 'a', newline='', encoding='utf-8') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow([str(id), vendor.text, name.text, price, pic_url])
+            id += 1
         
-        # write into csv
-        with open(csv_path, 'a', newline='', encoding='utf-8') as csvfile:
-            writer = csv.writer(csvfile)
-
-            # 寫入一列資料
-            writer.writerow([str(id), vendor.text, name.text, price, pic_url])
-        id += 1
-    
-    print(f"Id for next Round is: {id}")
-
+        print(f"Id for next Round is: {id}")
+    except AttributeError:
+        print(id)
 
 crawler(url, id)
